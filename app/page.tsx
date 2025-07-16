8ce7cf0f73511db2b7578ecbe9c8fcec99fc29dd
zorixid <elijah.dufour@outlook.com>
2025-07-16 15:51:40 -0400

    add auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,44 @@
-'use client'
+"use client";
 
-import { createClient } from '@/utils/supabase/client'
-// import { useRouter } from 'next/navigation';
-// import { useEffect } from 'react';
+import { createClient } from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Login() {
   const supabase = createClient();
-  // const router = useRouter();
+  const router = useRouter();
 
-  // useEffect(() => {
-  //   (async () => {
-  //     const { data } = await supabase.auth.getSession()
-  //     const owners = await prisma.owners.findMany({});
+  useEffect(() => {
+    (async () => {
+      const { data } = await supabase.auth.getSession();
+      const username = data?.session?.user?.user_metadata?.user_name;
 
-  //     if (data?.session && process.env.NEXT_PUBLIC_ADMIN_GITHUB_HANDLE?.split(",").includes(data?.session?.user?.user_metadata?.user_name)) {
-  //       router.push('/dashboard')
-  //     }
-  //   })();
-  // }, [])
+      if (
+        data?.session &&
+        process.env.NEXT_PUBLIC_ADMIN_GITHUB_HANDLE?.split(",").includes(
+          username
+        ) &&
+        username === "zorixid"
+      ) {
+        router.push("/dashboard");
+      }
+    })();
+  }, []);
 
   const loginHandler = async () => {
     await supabase.auth.signInWithOAuth({
-      provider: 'github',
-    })
-  }
+      provider: "github",
+    });
+  };
 
-  return <main className='flex justify-center items-center'>
-    <button className='px-5 py-2 rounded-md bg-white/10' onClick={loginHandler}>Login with Github</button>
-  </main>
+  return (
+    <main className="flex justify-center items-center">
+      <button
+        className="px-5 py-2 rounded-md bg-white/10"
+        onClick={loginHandler}
+      >
+        Login with Github
+      </button>
+    </main>
+  );
 }
